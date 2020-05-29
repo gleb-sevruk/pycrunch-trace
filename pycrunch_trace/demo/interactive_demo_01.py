cf365871408529e6065ecbbfeee7e088bafacdf0
Gleb Sevruk <gsev@diceus.com>
2020-05-29 11:02:20 +0300

    Demo rebalance, again

--- pycrunch_trace/demo/interactive_demo_01.py
+++ pycrunch_trace/demo/interactive_demo_01.py
@@ -13,7 +13,7 @@ def method_you_want_to_trace(some_number: int, some_string: str):
     # Take a look at the inspector panel.
     # Variable you passed, are recorded there.
     print('Press down arrow on keyboard to step into next line')
-    print('You can also use toolbar button (↓) near slider')
+    print('You can also use toolbar button [↓] near slider')
 
     print('Lets now change value of some_number.')
     some_number -= 1
@@ -25,7 +25,7 @@ def method_you_want_to_trace(some_number: int, some_string: str):
     # You can always go back using Left Arrow
     from_external_method = method_in_another_file(some_number)
 
-    print("If you haven't open graph yet, press G")
+    print("Press [G], if you haven't open graph yet, ")
     print("All toolbars can be accessed using settings button in the top right corner")
 
     print("Use [→] to `step into` next method")
@@ -34,9 +34,12 @@ def method_you_want_to_trace(some_number: int, some_string: str):
     alternative_ways_to_trace()
 
     print('You can start this tutorial again by dragging slider to the beginning')
+    print('  This is similar to scrolling through song in a music player or watching game replay')
 
     print("That's it. Start tracing now by installing:")
     print("  pip install pycrunch-trace")
+
+    one_last_thing('keyboard shortcuts')
 
     return str(datetime.utcnow())
 
@@ -47,19 +50,19 @@ def we_need_to_go_deeper():
     print("Lets take factorial for instance")
     sleep(0.1)
 
-    print("Click at `we_need_to_go_deeper` method in the graph, holding Command key")
+    print("Click at `we_need_to_go_deeper` method in the graph, holding [Command] key")
     # or (Control key on Windows)
     factorial = recursive_factorial(6)
 
     print("You can scope even deeper in call tree.")
-    print("  By clicking with the mouse while holding Command")
+    print("  By clicking with the mouse while holding [Command]")
 
     print("Entire call stack is drawn under the method you scoped to")
 
     print("You can exit from the scope same way:")
-    print(" By holding Command and clicking on outer method")
+    print(" By holding [Command] and clicking on outer method")
 
-    # Right arrow [→]
+    # Right arrow [→] to continue
     show_me_how_to_navigate_using_graph()
 
 
@@ -71,4 +74,48 @@ def recursive_factorial(num: int):
         return num * recursive_factorial(num - 1)
 
 
-method_you_want_to_trace(3, 'Welcome To PyTrace')
+def one_last_thing(dummy_variable):
+    sleep(0.1)
+    print('Most useful keyboard shortcuts:')
+    shortcuts = dict()
+    shortcuts['[G]'] = 'Toggle Graph View'
+    shortcuts['[i]'] = 'Toggle Inspector'
+
+    # Moving forward in debugger:
+    regular_debug = dict()
+    regular_debug['[→]'] = 'Next Event'
+    regular_debug['[↓]'] = 'Step Over; Ignoring method calls'
+    regular_debug['[Shift]+[↓]'] = 'Step Out of method'
+
+    # Moving back in debugger:
+    time_travel_debug = dict()
+    time_travel_debug['[←]'] = 'Previous Event'
+    time_travel_debug['[↑]'] = 'Step Over, in backward direction;'
+    time_travel_debug['[Shift]+[↑]'] = 'Step Out of method, backward direction, before call'
+
+    mutable_variable = 1
+    mutable_variable = example_of_step_out_back(mutable_variable + 1)
+
+    # Just use this:
+    #
+    #    [↑]
+    # [←][↓][→]
+    #
+    # instead of remembering all the F5...F10
+    return mutable_variable
+
+
+def example_of_step_out_back(mutable_variable: int):
+    print(mutable_variable)
+    mutable_variable += 1
+    print('Now try this: [Shift]+[↑]')
+    print('Step Out Back will bring you back, right before current function call.')
+    print('  When the `mutable_variable` was equal to 1')
+    print('  It is really useful if you need to re-enter method')
+    print('  Or check state before function call')
+    return mutable_variable + 1
+
+
+if __name__ == "__main__":
+    method_you_want_to_trace(3, 'Welcome To PyTrace')
+
--- pycrunch_trace/demo/interactive_demo_02.py
+++ pycrunch_trace/demo/interactive_demo_02.py
@@ -10,11 +10,12 @@ def method_in_another_file(some_number: int):
     print('Check this:')
     sleep(0.15)
 
-    print('You can see on the graph that we are at 300ms mark of the recording.')
+    print('You can see on the graph that we are at 150ms mark of the recording.')
     print('Current time (since recording start) is also displayed in inspector')
+    print('  And on graph panel as a needle')
 
-    print('You can skip next method call by using step over (Down Arrow)')
-    print('Or enter into by Right Arrow')
+    print('You can skip next method call by using Down Arrow [↓]')
+    print('Or enter into by Right Arrow [→]')
     dummy_method()
 
     print('Note __return pseudo-variable as we exit from this method')
@@ -22,8 +23,11 @@ def method_in_another_file(some_number: int):
 
 
 def dummy_method():
-    print('Notice `index` variable as you step through the loop')
+    print('See how `index` variable is changed as you step through the loop')
     for index in range(5):
         print('If you got trapped in a loop like this, use Shift+Down Arrow to step out of this method')
         print('You can also try Shift+Up Arrow to get to the place where this method was called')
         sleep(0.03)
+
+    # if you are not able to read all text, you can toggle inspector
+    #   by pressing [i]
