f338629160587d56c7d12f768eeb77e903df3454
Gleb Sevruk <gleb.sevruk@databand.ai>
2024-07-16 14:29:21 +0300

    Requirements pinned also in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from distutils.core import setup
 
 setup(name='pycrunch-trace',
-      version='0.2',
+      version='0.3',
       description='PyCrunch Time Travel Debugger',
       url='https://pytrace.com/',
       author='Gleb Sevruk',
@@ -21,7 +21,7 @@ setup(name='pycrunch-trace',
           'Cython',
           'jsonpickle',
           'PyYAML',
-          'protobuf>=3.11.3',
+          'protobuf>=3.11.3,<4',
           'jsonpickle',
       ],
       classifiers=[
